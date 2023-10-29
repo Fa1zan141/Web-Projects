bd9b792102126dada0d555282a1a8f60b7cf7eb8
MUHAMMAD FAIZAN <muhammad.faizan0141@gmail.com>
2023-10-29 21:33:00 +0500

    animated page

---
+++ Landing Page 6/Script.js
@@ -0,0 +1,23 @@
+gsap.from("#imgdiv1",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:60
+})
+gsap.from("#imgdiv2",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    x:60
+})
+gsap.from("#imgdiv3",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:-60
+})
+gsap.from("#text h1",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+})
